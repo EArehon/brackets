ae33e9a4c3320bfac2f78b2d5a0ba70f9e178cf4
EArehon <32142446+EArehon@users.noreply.github.com>
2020-03-03 14:47:08 +0200

    fead: add function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let a = str.split('');
+  
+  for(let i = 0; i < a.length; i++)
+  {
+    for(let j = 0; j < bracketsConfig.length; j++)
+    {
+    	if(a[i] == bracketsConfig[j][0] && a[i+1]== bracketsConfig[j][1]){
+      	a.splice(i, 2);
+        i-=2;
+        break;
+      }
+    }
+  }
+  
+  return a.length == 0 ? true : false;
 }
